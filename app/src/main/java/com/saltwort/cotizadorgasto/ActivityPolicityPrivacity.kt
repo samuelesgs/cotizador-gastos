761a6753ba956df67f04f88e6b25f57aefa77592
Samuel <69209156+samuelesgs@users.noreply.github.com>
2024-12-24 10:50:25 -0700

    v 1.2

---
+++ app/src/main/java/com/saltwort/cotizadorgasto/ActivityPolicityPrivacity.kt
@@ -0,0 +1,20 @@
+package com.saltwort.cotizadorgasto
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityPolicityPrivacity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_policity_privacity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
